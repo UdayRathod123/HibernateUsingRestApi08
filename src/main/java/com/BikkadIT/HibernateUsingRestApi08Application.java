7e96a739011ac4380a9463a626a75bbd17b795ac
unknown <udaysrathod95@gmail.com>
2022-08-12 23:07:30 +0530

    HRA-7 : create Hibernate config class with packege

---
+++ src/main/java/com/BikkadIT/HibernateUsingRestApi08Application.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateUsingRestApi08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateUsingRestApi08Application.class, args);
+	}
+
+}
